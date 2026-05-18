fedfd6fd57fcd5641bc32364e1574d9ba4065ea6
adityasetyawan22 <deriusaditya23@gmail.com>
2026-05-18 12:14:31 +0700

    Update KategoriAdapter.kt - code formatting

--- app/src/main/java/com/adit/penjualannya_adit/adapter/KategoriAdapter.kt
+++ app/src/main/java/com/adit/penjualannya_adit/adapter/KategoriAdapter.kt
@@ -3,7 +3,7 @@ package com.adit.penjualannya_adit.adapter
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
+import android.widget.ImageView      
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.adit.penjualannya_adit.Model.ModelKategori
